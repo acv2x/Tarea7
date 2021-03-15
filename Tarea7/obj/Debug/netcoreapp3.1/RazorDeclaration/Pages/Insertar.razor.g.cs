0030704be6c9ed78370ad288fc45782325c00217
ABel Batista Rodríguez <AbelBatiista@outlook.com>
2021-03-15 12:49:26 -0400

    tercera version

--- Tarea7/obj/Debug/netcoreapp3.1/RazorDeclaration/Pages/Insertar.razor.g.cs
+++ Tarea7/obj/Debug/netcoreapp3.1/RazorDeclaration/Pages/Insertar.razor.g.cs
@@ -13,91 +13,91 @@ namespace Tarea7.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\Willy Acevedo\Desktop\Programacion_3\Tarea7\Tarea7\_Imports.razor"
+#line 1 "C:\Users\abelb\Desktop\ITLA\CUATRIMESTRES\CUATRIMESTRE V\I. PROGRAMACIÓN III\MÓDULO VII - TEAMUP\III. ACTIVIDAD II\Tarea7\Tarea7\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Willy Acevedo\Desktop\Programacion_3\Tarea7\Tarea7\_Imports.razor"
+#line 2 "C:\Users\abelb\Desktop\ITLA\CUATRIMESTRES\CUATRIMESTRE V\I. PROGRAMACIÓN III\MÓDULO VII - TEAMUP\III. ACTIVIDAD II\Tarea7\Tarea7\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Willy Acevedo\Desktop\Programacion_3\Tarea7\Tarea7\_Imports.razor"
+#line 3 "C:\Users\abelb\Desktop\ITLA\CUATRIMESTRES\CUATRIMESTRE V\I. PROGRAMACIÓN III\MÓDULO VII - TEAMUP\III. ACTIVIDAD II\Tarea7\Tarea7\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Willy Acevedo\Desktop\Programacion_3\Tarea7\Tarea7\_Imports.razor"
+#line 4 "C:\Users\abelb\Desktop\ITLA\CUATRIMESTRES\CUATRIMESTRE V\I. PROGRAMACIÓN III\MÓDULO VII - TEAMUP\III. ACTIVIDAD II\Tarea7\Tarea7\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Willy Acevedo\Desktop\Programacion_3\Tarea7\Tarea7\_Imports.razor"
+#line 5 "C:\Users\abelb\Desktop\ITLA\CUATRIMESTRES\CUATRIMESTRE V\I. PROGRAMACIÓN III\MÓDULO VII - TEAMUP\III. ACTIVIDAD II\Tarea7\Tarea7\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Willy Acevedo\Desktop\Programacion_3\Tarea7\Tarea7\_Imports.razor"
+#line 6 "C:\Users\abelb\Desktop\ITLA\CUATRIMESTRES\CUATRIMESTRE V\I. PROGRAMACIÓN III\MÓDULO VII - TEAMUP\III. ACTIVIDAD II\Tarea7\Tarea7\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\Willy Acevedo\Desktop\Programacion_3\Tarea7\Tarea7\_Imports.razor"
+#line 7 "C:\Users\abelb\Desktop\ITLA\CUATRIMESTRES\CUATRIMESTRE V\I. PROGRAMACIÓN III\MÓDULO VII - TEAMUP\III. ACTIVIDAD II\Tarea7\Tarea7\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\Willy Acevedo\Desktop\Programacion_3\Tarea7\Tarea7\_Imports.razor"
+#line 8 "C:\Users\abelb\Desktop\ITLA\CUATRIMESTRES\CUATRIMESTRE V\I. PROGRAMACIÓN III\MÓDULO VII - TEAMUP\III. ACTIVIDAD II\Tarea7\Tarea7\_Imports.razor"
 using Tarea7;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\Willy Acevedo\Desktop\Programacion_3\Tarea7\Tarea7\_Imports.razor"
+#line 9 "C:\Users\abelb\Desktop\ITLA\CUATRIMESTRES\CUATRIMESTRE V\I. PROGRAMACIÓN III\MÓDULO VII - TEAMUP\III. ACTIVIDAD II\Tarea7\Tarea7\_Imports.razor"
 using Tarea7.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Willy Acevedo\Desktop\Programacion_3\Tarea7\Tarea7\Pages\Insertar.razor"
+#line 2 "C:\Users\abelb\Desktop\ITLA\CUATRIMESTRES\CUATRIMESTRE V\I. PROGRAMACIÓN III\MÓDULO VII - TEAMUP\III. ACTIVIDAD II\Tarea7\Tarea7\Pages\Insertar.razor"
 using Tarea7.Data;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Willy Acevedo\Desktop\Programacion_3\Tarea7\Tarea7\Pages\Insertar.razor"
+#line 3 "C:\Users\abelb\Desktop\ITLA\CUATRIMESTRES\CUATRIMESTRE V\I. PROGRAMACIÓN III\MÓDULO VII - TEAMUP\III. ACTIVIDAD II\Tarea7\Tarea7\Pages\Insertar.razor"
 using System.Data.SqlClient;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Willy Acevedo\Desktop\Programacion_3\Tarea7\Tarea7\Pages\Insertar.razor"
+#line 4 "C:\Users\abelb\Desktop\ITLA\CUATRIMESTRES\CUATRIMESTRE V\I. PROGRAMACIÓN III\MÓDULO VII - TEAMUP\III. ACTIVIDAD II\Tarea7\Tarea7\Pages\Insertar.razor"
 using System.Net;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Willy Acevedo\Desktop\Programacion_3\Tarea7\Tarea7\Pages\Insertar.razor"
+#line 5 "C:\Users\abelb\Desktop\ITLA\CUATRIMESTRES\CUATRIMESTRE V\I. PROGRAMACIÓN III\MÓDULO VII - TEAMUP\III. ACTIVIDAD II\Tarea7\Tarea7\Pages\Insertar.razor"
 using Newtonsoft.Json;
 
 #line default
@@ -112,7 +112,7 @@ using Newtonsoft.Json;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 51 "C:\Users\Willy Acevedo\Desktop\Programacion_3\Tarea7\Tarea7\Pages\Insertar.razor"
+#line 51 "C:\Users\abelb\Desktop\ITLA\CUATRIMESTRES\CUATRIMESTRE V\I. PROGRAMACIÓN III\MÓDULO VII - TEAMUP\III. ACTIVIDAD II\Tarea7\Tarea7\Pages\Insertar.razor"
        
     string Nombre, Apellido, Cedula, SignoZodiacal, Vacuna_Recibida;
     string exito, Primera_Dosis, Segunda_Dosis;
--- Tarea7/obj/Debug/netcoreapp3.1/RazorDeclaration/_Imports.razor.g.cs
+++ Tarea7/obj/Debug/netcoreapp3.1/RazorDeclaration/_Imports.razor.g.cs
@@ -13,63 +13,63 @@ namespace Tarea7
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\Willy Acevedo\Desktop\Programacion_3\Tarea7\Tarea7\_Imports.razor"
+#line 1 "C:\Users\abelb\Desktop\ITLA\CUATRIMESTRES\CUATRIMESTRE V\I. PROGRAMACIÓN III\MÓDULO VII - TEAMUP\III. ACTIVIDAD II\Tarea7\Tarea7\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Willy Acevedo\Desktop\Programacion_3\Tarea7\Tarea7\_Imports.razor"
+#line 2 "C:\Users\abelb\Desktop\ITLA\CUATRIMESTRES\CUATRIMESTRE V\I. PROGRAMACIÓN III\MÓDULO VII - TEAMUP\III. ACTIVIDAD II\Tarea7\Tarea7\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Willy Acevedo\Desktop\Programacion_3\Tarea7\Tarea7\_Imports.razor"
+#line 3 "C:\Users\abelb\Desktop\ITLA\CUATRIMESTRES\CUATRIMESTRE V\I. PROGRAMACIÓN III\MÓDULO VII - TEAMUP\III. ACTIVIDAD II\Tarea7\Tarea7\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Willy Acevedo\Desktop\Programacion_3\Tarea7\Tarea7\_Imports.razor"
+#line 4 "C:\Users\abelb\Desktop\ITLA\CUATRIMESTRES\CUATRIMESTRE V\I. PROGRAMACIÓN III\MÓDULO VII - TEAMUP\III. ACTIVIDAD II\Tarea7\Tarea7\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Willy Acevedo\Desktop\Programacion_3\Tarea7\Tarea7\_Imports.razor"
+#line 5 "C:\Users\abelb\Desktop\ITLA\CUATRIMESTRES\CUATRIMESTRE V\I. PROGRAMACIÓN III\MÓDULO VII - TEAMUP\III. ACTIVIDAD II\Tarea7\Tarea7\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Willy Acevedo\Desktop\Programacion_3\Tarea7\Tarea7\_Imports.razor"
+#line 6 "C:\Users\abelb\Desktop\ITLA\CUATRIMESTRES\CUATRIMESTRE V\I. PROGRAMACIÓN III\MÓDULO VII - TEAMUP\III. ACTIVIDAD II\Tarea7\Tarea7\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\Willy Acevedo\Desktop\Programacion_3\Tarea7\Tarea7\_Imports.razor"
+#line 7 "C:\Users\abelb\Desktop\ITLA\CUATRIMESTRES\CUATRIMESTRE V\I. PROGRAMACIÓN III\MÓDULO VII - TEAMUP\III. ACTIVIDAD II\Tarea7\Tarea7\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\Willy Acevedo\Desktop\Programacion_3\Tarea7\Tarea7\_Imports.razor"
+#line 8 "C:\Users\abelb\Desktop\ITLA\CUATRIMESTRES\CUATRIMESTRE V\I. PROGRAMACIÓN III\MÓDULO VII - TEAMUP\III. ACTIVIDAD II\Tarea7\Tarea7\_Imports.razor"
 using Tarea7;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\Willy Acevedo\Desktop\Programacion_3\Tarea7\Tarea7\_Imports.razor"
+#line 9 "C:\Users\abelb\Desktop\ITLA\CUATRIMESTRES\CUATRIMESTRE V\I. PROGRAMACIÓN III\MÓDULO VII - TEAMUP\III. ACTIVIDAD II\Tarea7\Tarea7\_Imports.razor"
 using Tarea7.Shared;
 
 #line default
